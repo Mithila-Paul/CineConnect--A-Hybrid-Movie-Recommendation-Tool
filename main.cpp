c7836c443e60937560cbaed492ecad86ecdd71b8
Mithila-Paul <mithimithi220@gmail.com>
2026-01-12 08:53:16 +0600

    Enhance user interaction messages in main.cpp
    Updated welcome message and exit message for user interaction.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include "User.h"
+
 using namespace std;
 
 int main() {
     int choice;
-    while(true) {
-        cout << "\nWelcome to CineConnect! \n";
-        cout << "Kindly Select your \n";
+
+    while (true) {
+        cout << "\nWelcome To CineConnect!\n";
+        cout << "What is better than watching movies? DUH! Quickly Tell me what you want to do?\n";
         cout << "1. Register\n";
         cout << "2. Login\n";
         cout << "3. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
 
-        if(choice == 1) registerUser();
-        else if(choice == 2) loginUser();
-        else break;
+        if (choice == 1) registerUser();
+        else if (choice == 2) loginUser();
+        else
+            { cout << "Bye Bye, will miss you!";
+              break;
+            }
     }
     return 0;
 }
