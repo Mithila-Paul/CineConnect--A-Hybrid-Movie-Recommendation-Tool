f04bc7b8985150c4580cfe20889189d6984c817c
Mithila-Paul <mithimithi220@gmail.com>
2026-01-12 08:49:43 +0600

    Refactor User.h to declare functions

--- User.h
+++ User.h
@@ -1,53 +1,15 @@
-#include <iostream>
-using namespace std;
+#ifndef USER_H
+#define USER_H
 
-    void registerUser() {
-    string uid, name;
-    cout << "Enter new User ID: ";
-    cin >> uid;
+#include <string>
 
-    ofstream fout("user_" + uid + ".txt");
-    fout << uid << endl;
-    cout << "Enter Name: ";
-    cin >> name;
-    fout << name << endl;
-    fout.close();
+void registerUser();
+bool loginUser();
+void userMenu(int userId);
 
-    cout << "Registration Successful!" << endl;
-  }
+//menu 
+void rateMovie(int userId);
+void showMyRatings(int userId);
+void recommendMoviesForUser(int userId);
 
-  bool loginUser() {
-    string uid;
-    cout << "Enter User ID: ";
-    cin >> uid;
-
-    ifstream fin("user_" + uid + ".txt");
-    if(!fin) {
-        cout << "User not found!\n";
-        return false;
-    }
-    cout << "Login Successful!" << endl;
-
-    userMenu(uid);
-    fin.close();
-    return true;
-  }
-
-  void userMenu(string uid) {
-    int c;
-    while(true) {
-        cout << "\n User Menu: \n";
-        cout << "1. Rate a Movie\n";
-        cout << "2. See My Ratings\n";
-        cout << "3. Recommend Movies\n";
-        cout << "4. Statistics\n";
-        cout << "5. Logout\n";
-        cin >> c;
-
-        if(c == 1) rateMovie(uid);
-        else if(c == 2) showMyRatings(uid);
-        else if(c == 3) recommend(uid);
-        else if(c == 4) showStats();
-        else break;
-    }
-}
+#endif
