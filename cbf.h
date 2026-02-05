93f8c12114a3bf941c23ce583ec4f90c295e746d
Mithila-Paul <mithimithi220@gmail.com>
2026-02-05 15:04:27 +0600

    Enhance cbf.h with additional movie recommendation features
    Added new functions for loading movie topics and building query vectors.

--- cbf.h
+++ cbf.h
@@ -7,33 +7,46 @@
 #include <unordered_set>
 
 std::unordered_set<int>
-getRatedMovieIds(const std::string& ratingsFile); 
+getRatedMovieIds(const std::string &ratingsFile);
 
 std::unordered_map<int, std::string>
-loadMovieTitles(const std::string& csvFile); 
+loadMovieTitles(const std::string &csvFile);
 
-//defining vectors to avoid compilation issues
+// defining vectors to avoid compilation issues
 using Vector = std::unordered_map<std::string, double>;
 
 std::vector<int> coldStartRecommend(
-    const std::vector<std::pair<int, Vector>>& movieVectors,
-    int topN
-);
+    const std::vector<std::pair<int, Vector>> &movieVectors,
+    const Vector &queryVector,
+    int topN);
 
 std::vector<std::pair<int, Vector>>
-loadMovieVectors(const std::string& filename);
+loadMovieVectors(const std::string &filename);
 
 Vector buildUserVector(
-    const std::string& ratingsFile,
-    const std::vector<std::pair<int, Vector>>& movieVectors
-);
+    const std::string &ratingsFile,
+    const std::vector<std::pair<int, Vector>> &movieVectors);
 
-double cosineSimilarity(const Vector& A, const Vector& B);
+double cosineSimilarity(const Vector &A, const Vector &B);
+
+std::unordered_map<int, std::vector<std::string>> loadMovieTopics(const std::string &file);
+
+Vector buildQueryVector(
+    const std::string &query,
+    const std::unordered_map<std::string, int> &df,
+    int totalDocs);
 
 std::vector<int> recommendMovies(
-    const Vector& userVector,
-    const std::vector<std::pair<int, Vector>>& movieVectors,
-    int topN, const std::unordered_set<int>& ratedMovies
+    const Vector &userVector,
+    const Vector &queryVector,
+    const std::vector<std::pair<int, Vector>> &movieVectors,
+    int topN,
+    const std::unordered_set<int> &ratedMovies,
+    const std::vector<std::string> &queryTopics // The 6th argument
 );
+
+double topicSimilarity(
+    const std::vector<std::string> &queryTopics,
+    const std::vector<std::string> &movieTopics);
 
 #endif
