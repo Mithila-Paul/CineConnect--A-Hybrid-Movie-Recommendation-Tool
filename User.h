87ae439c5ad58a7bcb1558fbafaf199d834a29c7
Mithila-Paul <mithimithi220@gmail.com>
2026-04-18 11:03:41 +0600

    Add rating helper functions to User.h

--- User.h
+++ User.h
@@ -12,4 +12,11 @@ void rateMovie(int userId);
 void showMyRatings(int userId);
 void recommendMoviesForUser(int userId);
 
+// rating helpers (used internally, exposed for CF/MF integration later)
+bool hasRatedMovie(int userId, int movieId);
+void appendToGlobalRatings(int userId, int movieId, double rating);
+void removeFromGlobalRatings(int userId, int movieId);
+void removeFromUserFile(int userId, int movieId);
+
+
 #endif
