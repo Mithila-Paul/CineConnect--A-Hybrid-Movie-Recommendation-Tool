6b960de94db1042140f2aeafee896b336bd52a96
Mithila-Paul <mithimithi220@gmail.com>
2026-04-21 09:39:37 +0600

    Add cold-start onboarding helper functions

--- User.h
+++ User.h
@@ -18,5 +18,11 @@ void appendToGlobalRatings(int userId, int movieId, double rating);
 void removeFromGlobalRatings(int userId, int movieId);
 void removeFromUserFile(int userId, int movieId);
 
+// cold-start onboarding helpers
+void runColdStartOnboarding(int userId);
+std::vector<std::string> chooseFavoriteGenres(int maxGenres = 3);
+std::vector<int> getTopSeedMoviesByGenre(const std::string &genre, int count = 5);
+std::string getGenreDisplayName(const std::string &genreKey);
+
 
 #endif
