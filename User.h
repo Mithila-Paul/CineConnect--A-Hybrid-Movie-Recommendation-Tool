89df3afb5be72886bf531a30ddbe6a784354d13c
Mithila-Paul <mithimithi220@gmail.com>
2026-04-23 14:09:21 +0600

    Update User.h to include TrendsData in functions

--- User.h
+++ User.h
@@ -2,27 +2,29 @@
 #define USER_H
 
 #include <string>
+#include <vector>
+#include "trends.h"
 
-void registerUser();
-bool loginUser();
-void userMenu(int userId);
+void registerUser(const TrendsData &trends);
+bool loginUser(const TrendsData &trends);
 
-//menu 
+void userMenu(int userId, const TrendsData &trends);
+
 void rateMovie(int userId);
 void showMyRatings(int userId);
-void recommendMoviesForUser(int userId);
+void recommendMoviesForUser(int userId, const TrendsData &trends);
 
-// rating helpers (used internally, exposed for CF/MF integration later)
 bool hasRatedMovie(int userId, int movieId);
 void appendToGlobalRatings(int userId, int movieId, double rating);
 void removeFromGlobalRatings(int userId, int movieId);
 void removeFromUserFile(int userId, int movieId);
 
-// cold-start onboarding helpers
-void runColdStartOnboarding(int userId);
-std::vector<std::string> chooseFavoriteGenres(int maxGenres = 3);
-std::vector<int> getTopSeedMoviesByGenre(const std::string &genre, int count = 5);
-std::string getGenreDisplayName(const std::string &genreKey);
+void runColdStartOnboarding(int userId, const TrendsData &trends);
 
+std::vector<std::string> chooseFavoriteGenres(
+    int maxGenres = 3,
+    const TrendsData *trends = nullptr);
+
+std::string getGenreDisplayName(const std::string &genreKey);
 
 #endif
