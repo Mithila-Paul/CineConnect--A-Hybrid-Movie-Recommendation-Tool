17694281f980d40b9ea19ae40ce6cb22a8403e5e
Mithila-Paul <mithimithi220@gmail.com>
2026-04-23 14:00:36 +0600

    Refactor cbf.h
    Updated function signatures to include userId parameter and added processQuery function.

--- cbf.h
+++ cbf.h
@@ -6,35 +6,44 @@
 #include <string>
 #include <unordered_set>
 
-std::unordered_set<int>
-getRatedMovieIds(const std::string &ratingsFile);
-
-std::unordered_map<int, std::string>
-loadMovieTitles(const std::string &csvFile);
-
-// defining vectors to avoid compilation issues
 using Vector = std::unordered_map<std::string, double>;
-
-std::vector<int> coldStartRecommend(
-    const std::vector<std::pair<int, Vector>> &movieVectors,
-    const Vector &queryVector,
-    int topN);
 
 std::vector<std::pair<int, Vector>>
 loadMovieVectors(const std::string &filename);
 
+std::unordered_map<int, std::string>
+loadMovieTitles(const std::string &csvFile);
+
+std::unordered_map<int, std::vector<std::string>>
+loadMovieTopics(const std::string &file);
+
+std::unordered_set<int>
+getRatedMovieIds(const std::string &ratingsFile, int userId);
+
 Vector buildUserVector(
     const std::string &ratingsFile,
-    const std::vector<std::pair<int, Vector>> &movieVectors);
-
-double cosineSimilarity(const Vector &A, const Vector &B);
-
-std::unordered_map<int, std::vector<std::string>> loadMovieTopics(const std::string &file);
+    const std::vector<std::pair<int, Vector>> &movieVectors,
+    int userId);
 
 Vector buildQueryVector(
     const std::string &query,
     const std::unordered_map<std::string, int> &df,
     int totalDocs);
+
+std::vector<std::string> processQuery(std::string query);
+
+double cosineSimilarity(const Vector &A, const Vector &B);
+
+double topicSimilarity(
+    const std::vector<std::string> &queryTopics,
+    const std::vector<std::string> &movieTopics);
+
+bool isColdStart(const Vector &userVector);
+
+std::vector<int> coldStartRecommend(
+    const std::vector<std::pair<int, Vector>> &movieVectors,
+    const Vector &queryVector,
+    int topN);
 
 std::vector<int> recommendMovies(
     const Vector &userVector,
@@ -42,11 +51,6 @@ std::vector<int> recommendMovies(
     const std::vector<std::pair<int, Vector>> &movieVectors,
     int topN,
     const std::unordered_set<int> &ratedMovies,
-    const std::vector<std::string> &queryTopics // The 6th argument
-);
-
-double topicSimilarity(
-    const std::vector<std::string> &queryTopics,
-    const std::vector<std::string> &movieTopics);
+    const std::vector<std::string> &queryTopics);
 
 #endif
