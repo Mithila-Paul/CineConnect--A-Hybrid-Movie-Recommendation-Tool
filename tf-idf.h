c5b32814366538f28ac72218d0449604ea4885a2
Mithila-Paul <mithimithi220@gmail.com>
2026-04-23 12:46:31 +0600

    Refactor TF-IDF header with new functions and defaults
    Updated 'extractMovieTopics' to allow topWords parameter to default to 15. Added functions to initialize TF-IDF cache and retrieve global document frequency and total document count.

--- tf-idf.h
+++ tf-idf.h
@@ -9,16 +9,12 @@
 
 using namespace std;
 
-// Type alias for sparse TF-IDF vectors
 using Vector = unordered_map<string, double>;
 
-// Tokenization / preprocessing
 vector<string> tokenize(const string &text);
 
-// CSV reading
 vector<pair<int, string>> readMoviesFromCSV(const string &filename);
 
-// TF / DF / TF-IDF
 unordered_map<string, double> computeTF(const vector<string> &words);
 unordered_map<string, int> computeDF(const vector<vector<string>> &docs);
 unordered_map<string, double> computeTFIDF(
@@ -26,23 +22,23 @@ unordered_map<string, double> computeTFIDF(
     const unordered_map<string, int> &df,
     int totalDocs);
 
-// Save generated movie vectors
 void saveMovieVectorsToFile(
     const vector<pair<int, unordered_map<string, double>>> &movieVectors,
     const string &filename);
 
-// Topic extraction and saving
 unordered_map<int, vector<string>> extractMovieTopics(
     const vector<pair<int, unordered_map<string, double>>> &movieVectors,
-    int topWords = 5);
+    int topWords = 15);
 
 void saveTopicsToFile(
     const unordered_map<int, vector<string>> &topics);
 
-// Genre file generation
 void generateGenreFile(const string &csvFile, const string &outFile);
 
-// System initialization
-void initializeSystem();
+void initializeTFIDFCache();
+
+const std::unordered_map<std::string, int> &getGlobalDF();
+
+int getGlobalTotalDocs();
 
 #endif
